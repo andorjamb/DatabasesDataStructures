e036890a42410f77fe5cd06c7de5fb01ee17500c
Anna Petelin <andorjamb@protonmail.com>
2023-01-06 14:53:35 +0200

    stack push and pop working

--- stack-task/src/App.js
+++ stack-task/src/App.js
@@ -7,10 +7,12 @@ const App = () => {
 
   const [randoms, setRandoms] = useState([]);
   const [stack, setStack] = useState([]);
+  const [text, setText] = useState("");
 
   let randomArray = [];
 
   const newArray = () => {
+    setText("");
     randomArray = [];
     for (let i = 0; i < 10; i++) {
       let newNumber = Math.floor(Math.random() * 100);
@@ -20,14 +22,27 @@ const App = () => {
   }
 
   function pushToStack() {
+    if (text !== "") { setText(""); }
+    if (randoms.length === 0) {
+      setText("You are out of numbers! Get some more.");
+      return null;
+    }
     let number = randoms.pop()
-    console.log(number);
     let stackArray = [...stack, number];
     setStack(stackArray);
 
   }
 
   function popFromStack() {
+    if (stack.length === 0) {
+      setText("Nothing left to pop. Push some more numbers.")
+      return null;
+    }
+    let stackArray = [...stack];
+    console.log(stackArray);
+    stackArray.pop();
+    console.log(stackArray);
+    setStack(stackArray);
 
   }
 
@@ -39,14 +54,16 @@ const App = () => {
     <div className="App" >
       <header><div className="title-container"><h1>Stack App</h1></div></header>
       <main className="container">
-        <button id="getNumbers" onClick={newArray}>Get numbers</button>
+        <div className='button-container'><button id="getNumbers" onClick={newArray}>Get numbers</button></div>
         <h3>Some random numbers: </h3>
         <DisplayArray array={randoms} />
         <h3>The stack:</h3>
         <DisplayArray array={stack} />
         <h3>Push a new random number to the stack or remove a number:</h3>
-        <button id="push" onClick={pushToStack}>Push</button>
-        <button id="pop" onClick={popFromStack}>Pop</button>
+        <span className="text">{text}</span>
+        <div className='button-container'> <button id="push" onClick={pushToStack}>Push</button>
+          <button id="pop" onClick={popFromStack}>Pop</button></div>
+
       </main>
     </div>
   );
